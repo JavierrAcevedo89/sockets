7f1cccbfd98024b3fce91ba0d66e02435f48e4d5
Javier Acevedo <javier.acevedohdz@hotmail.com>
2024-05-14 10:17:45 -0600

    Funge como versión funcional Versión funcionando al 100%.

--- public/main.js
+++ public/main.js
@@ -1,32 +1,34 @@
 var socket = io.connect('http://localhost:3005', {'forceNew': true});
 
-/*El cliente manejara datos mediante mensajes, esto se llamaran eventos y 
-se mostraran por consola en el navegador*/
+/*El cliente manejara datos mediante mensajes, esto se llamara eventos y se mostraran por la consola en el navergador */
 socket.on('messages', function(data){
     console.log(data);
     render(data);
 });
-/*Creamos un template para que nos imprima el entendi*/
+
 function render(data){
-    //aqui se inicia el manejo de string que viene en EM6 se usan estas comillas
-    //las variables se colocan con el signo de $ y entre {}
+    //Restructuramos esta seccion para que se maneje el array
+    //elem es un conjunto de cosas
+    //con ma recorremos el array
 
-    var html= data.map(function(elem, index){
-    return(`<div>
-        <strong>${elem.autor}</strong>:
-        <em>${elem.texto}</em>
-    </div>`);
+    var html = data.map(function(elem, index){
+            return(`<div>
+                    <strong>${elem.autor}</strong>:
+                    <em>${elem.texto}</em>
+                    </div>`);
     }).join(" ");
-    
-    document.getElementById('messages').innerHTML=html;
-}
-// cada ves que alguien precione el boton enviar en el formulario
-// el cliente amite un nuevo mensaje y manda el payload
 
+                document.getElementById('messages').innerHTML = html;
+}
+
+//Cada ves que alguien precione el boton enviar en el formulario
+//El cliente emite un nuevo mensaje y manda el payload
+//Culminar el proyecto a una version Beta
 function addMessage(e){
-    var payload={
+    var payload = {
         autor: document.getElementById('username').value,
         texto: document.getElementById('texto').value
+
     };
     socket.emit('new-message', payload);
     return false;
--- server/main.js
+++ server/main.js
@@ -2,25 +2,31 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-//array qeu guarda los mensajes
+//Array  que guarda los mensajes
 var messages = [{
-    id:1,
-        texto:"K rollo con el pollo",
-        autor: "Jabi Acevedo"
+    id: 1,
+    texto: "Q rollo con el pollo",
+    autor: "Jabi Acevedo"
 }];
+
+/* Usamos un middleware para usar elementos estáticos en la sección pública de la aplicación */
 app.use(express.static('public'));
 
 app.get('/',function(req, res){
     res.status(200).send("Hola Mundo");
 });
 
+/* De esta forma activamos socket para que esté escuchando 
+mandamos un mensaje de control por consola para saber que pasa
+y tenemos que hacer el mensaje venga del navegador web mediante HTML y JS */
 io.on('connection', function(socket){
     console.log('Alguien se ha conectado con socket')
-   socket.emit('messages', messages);
-    socket.on('new-message',function(dat){
-        message.push(data);
+    socket.emit('messages', messages);
+    //ahora queremos escuchar los mensajes mandados por el cliente
+    socket.on('new-message', function(data){
+        messages.push(data);
         //queremos que todos los mensajes se manden a todos los clientes
-        io.socket.emit('messages', messages);
+        io.sockets.emit('messages', messages);
     });
 });
 
