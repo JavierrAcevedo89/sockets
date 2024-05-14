fc0bd5dc166d5fc428dedc35f75402dfddc0af5e
Javier Acevedo <javier.acevedohdz@hotmail.com>
2024-05-14 09:47:34 -0600

    Funge como versión funcional Versión 4.0 que tiene el mismo código de la rama master pero este código sin funciona.

--- public/main.js
+++ public/main.js
@@ -1,1 +1,33 @@
-var socket = io.connect('http://localhost:3002', {'forceNew': true});
+var socket = io.connect('http://localhost:3005', {'forceNew': true});
+
+/*El cliente manejara datos mediante mensajes, esto se llamaran eventos y 
+se mostraran por consola en el navegador*/
+socket.on('messages', function(data){
+    console.log(data);
+    render(data);
+});
+/*Creamos un template para que nos imprima el entendi*/
+function render(data){
+    //aqui se inicia el manejo de string que viene en EM6 se usan estas comillas
+    //las variables se colocan con el signo de $ y entre {}
+
+    var html= data.map(function(elem, index){
+    return(`<div>
+        <strong>${elem.autor}</strong>:
+        <em>${elem.texto}</em>
+    </div>`);
+    }).join(" ");
+    
+    document.getElementById('messages').innerHTML=html;
+}
+// cada ves que alguien precione el boton enviar en el formulario
+// el cliente amite un nuevo mensaje y manda el payload
+
+function addMessage(e){
+    var payload={
+        autor: document.getElementById('username').value,
+        texto: document.getElementById('texto').value
+    };
+    socket.emit('new-message', payload);
+    return false;
+}
--- server/main.js
+++ server/main.js
@@ -2,19 +2,26 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
-/* Usamos un middleware para usar elementos estáticos en la sección pública de la aplicación */
+//array qeu guarda los mensajes
+var messages = [{
+    id:1,
+        texto:"K rollo con el pollo",
+        autor: "Jabi Acevedo"
+}];
 app.use(express.static('public'));
 
 app.get('/',function(req, res){
     res.status(200).send("Hola Mundo");
 });
 
-/* De esta forma activamos socket para que esté escuchando 
-mandamos un mensaje de control por consola para saber que pasa
-y tneemos que hacer el mensaje venga del navegador web mediante HTML y JS */
 io.on('connection', function(socket){
     console.log('Alguien se ha conectado con socket')
+   socket.emit('messages', messages);
+    socket.on('new-message',function(dat){
+        message.push(data);
+        //queremos que todos los mensajes se manden a todos los clientes
+        io.socket.emit('messages', messages);
+    });
 });
 
 server.listen(3002, function(){
